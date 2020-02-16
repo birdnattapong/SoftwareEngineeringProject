ab9c24ab0f6a89856ec66dd1f85e32e433aa04f5
rakchanok <popsms1@gmail.com>
2020-02-16 21:40:38 +0700

    [CREATE] header.php

--- header.php
+++ header.php
@@ -2,9 +2,48 @@
 <html>
     <heade>
         <title>Test</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+        /* Note: Try to remove the following lines to see the effect of CSS positioning */
+        .affix {
+            top: 0;
+            width: 100%;
+            z-index: 9999 !important;
+        }
+
+        .affix + .container-fluid {
+            padding-top: 70px;
+        }
+        </style>
     </heade>
     <body>
-            <div id="role_div"></div>
-            <input type="button" id="role_button" value="Role">
+
+        <center>
+            <table style="background:none url(http://i102.photobucket.com/albums/m108/chingdong/blackmoonglitter03_tlg.gif);";cellpadding="10" cellspacing="8">
+            <tr><td><table style="background:none url(http://i102.photobucket.com/albums/m108/chingdong/aadiamondz01b-1.gif);";cellpadding="9" cellspacing="8">
+            <tr><td><table style='background:none url(http://i102.photobucket.com/albums/m108/chingdong/blackmoonglitter03_tlg.gif);';cellpadding="8" cellspacing="8">
+            <tr><td><table style='background:#9ACD32 ;cellpadding='7' cellspacing="5"; width=1366>
+            <tr><td> <br><p align="center">
+            <b><font size="1">
+                <div id="role_div"></div>
+                <h3 align = 'right'>
+                    <input type="button" id="role_button" value="Role">
+                </h3>
+            </font></b><br></td></tr>
+            </table>
+            </td></tr>
+            </table>
+            </td></tr>
+            </table>
+            </td></tr>
+            </table>
+            </center>
+
+            
+
     </body>
 </html>
